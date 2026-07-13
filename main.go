e526289cc48f882391370be457f50250d1e509e5
Dimitris Dalianis <gbmemc1@proton.me>
2026-07-13 17:35:26 +0200

    session-1: Run jobs once

--- main.go
+++ main.go
@@ -1,5 +1,79 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+)
 
+type Target struct {
+	Name            string `json:"name"`
+	Url             string `json:"url"`
+	IntervalSeconds int    `json:"interval_seconds"`
+}
+
+type Result struct {
+	Target     string
+	StatusCode int
+	Latency    time.Duration
+	Err        error
+	When       time.Time
+}
+
+func worker(id int, jobs <-chan Target, results chan<- Result) {
+	for j := range jobs {
+		fmt.Println("Worker", id, "started job", j)
+		started := time.Now()
+		resp, err := http.Get(j.Url)
+		result := Result{
+			Target:  j.Name,
+			Latency: time.Since(started),
+			Err:     err,
+			When:    started,
+		}
+		if err == nil {
+			result.StatusCode = resp.StatusCode
+			resp.Body.Close()
+		}
+		results <- result
+	}
+}
+
+func main() {
+	var targets []Target
+	targetsRaw, err := os.ReadFile("targets.json")
+	if err != nil {
+		panic(err)
+	}
+	if err := json.Unmarshal(targetsRaw, &targets); err != nil {
+		panic(err)
+	}
+
+	jobs := make(chan Target)
+	results := make(chan Result)
+
+	var wg sync.WaitGroup
+
+	for i := range 2 {
+		wg.Go(func() {
+			worker(i, jobs, results)
+		})
+	}
+
+	wg.Go(func() {
+		for range targets {
+			fmt.Println(<-results)
+		}
+	})
+
+	for i, t := range targets {
+		jobs <- t
+		fmt.Println("Pushed job", i)
+		close(jobs)
+	}
+	wg.Wait()
+	fmt.Println("Bye!")
 }
